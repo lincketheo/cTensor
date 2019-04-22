34041af9b78e28a86f8f8c70e4cd92a52e483207
tlincke125 <tlincke125@gmail.com>
2019-04-22 15:14:02 -0600

    ready to submit

--- src/runTheDamnNetwork.cpp
+++ src/runTheDamnNetwork.cpp
@@ -22,21 +22,19 @@ int main(int argc, char ** argv){
     const char * fileNames[10] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
     int labels[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}; 
 
-    trainingSet testing = uploadAllImages(fileBaseCONSTANT, fileNames, labels, 10, "jpg", 10000);
+    trainingSet testing = uploadAllImages(fileBaseCONSTANT, fileNames, labels, 10, "jpg", 10);
 
     int nHL[100] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
     int sHL[100] = {400, 100, 400, 400, 400, 100, 100, 100, 50, 50, 50, 200, 200, 200};
     float rate[100] = {.008, .008, .009, .007, .008, .009, .007, .008, .009, .007, .008, .009, .007};
-    trainingSet testing2 = uploadAllImages(fileBaseTestCONSTANT, fileNames, labels, 10, "jpg", 10000);
+    trainingSet testing2 = uploadAllImages(fileBaseTestCONSTANT, fileNames, labels, 10, "jpg", 20);
 
 
     for(int i = 0; i < 15; i++){
         Network * temp = trainDataSet(testing, nHL[i], sHL[i], rate[i]);
         float result = testOnTestSet(testing2, temp); 
-        std::cout<<"Layers = "<<nHL[i]<<" Rate = "<<rate[i]<<" nodes = "<<sHL[i]<<std::endl;
-        std::cout<<"Accuracy = "<<result<<std::endl;
+        std::cout<<result<<std::endl;
     }
-
     return 0;
 }
 
