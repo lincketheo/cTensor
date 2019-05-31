6a626dc83d867e6d836b85d3250cc3519c021887
tlincke125 <thli2739@colorado.edu>
2019-05-30 21:52:23 -0600

    added builder

---
+++ builds/CMakeFiles/run.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/theo/dev/projects/math/cTensor/src/Data/dataHandler.cpp" "/home/theo/dev/projects/math/cTensor/builds/CMakeFiles/run.dir/Data/dataHandler.cpp.o"
+  "/home/theo/dev/projects/math/cTensor/src/Matrix/kernel.cpp" "/home/theo/dev/projects/math/cTensor/builds/CMakeFiles/run.dir/Matrix/kernel.cpp.o"
+  "/home/theo/dev/projects/math/cTensor/src/Matrix/operations.cpp" "/home/theo/dev/projects/math/cTensor/builds/CMakeFiles/run.dir/Matrix/operations.cpp.o"
+  "/home/theo/dev/projects/math/cTensor/src/Networks/Network.cpp" "/home/theo/dev/projects/math/cTensor/builds/CMakeFiles/run.dir/Networks/Network.cpp.o"
+  "/home/theo/dev/projects/math/cTensor/src/UI/driver.cpp" "/home/theo/dev/projects/math/cTensor/builds/CMakeFiles/run.dir/UI/driver.cpp.o"
+  "/home/theo/dev/projects/math/cTensor/src/main.cpp" "/home/theo/dev/projects/math/cTensor/builds/CMakeFiles/run.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/theo/dev/projects/math/cTensor/src/../include"
+  "/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ builds/CMakeFiles/run.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/run.dir/main.cpp.o"
+  "CMakeFiles/run.dir/UI/driver.cpp.o"
+  "CMakeFiles/run.dir/Matrix/kernel.cpp.o"
+  "CMakeFiles/run.dir/Matrix/operations.cpp.o"
+  "CMakeFiles/run.dir/Networks/Network.cpp.o"
+  "CMakeFiles/run.dir/Data/dataHandler.cpp.o"
+  "run.pdb"
+  "run"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/run.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
