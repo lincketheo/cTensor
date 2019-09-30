11c0fda8343f6814cb9cb6d4e90a18fd56105d8f
tlincke125 <thli2739@colorado.edu>
2019-09-30 15:45:08 -0600

    adjusted matrix interface base

--- include/linearalg/MatrixInterface.hpp
+++ include/linearalg/MatrixInterface.hpp
@@ -8,48 +8,92 @@
 
 #define MATRIXINTERFACE_HPP
 
+struct dim
+{
+  dim(const size_t x_, const size_t y_)
+    : x(_x)
+    , y(_y)
+  {}
+  size_t x;
+  size_t y;
+};
 
-template <typename T>
+template<typename T>
 class MatrixInterface
 {
-    public:
-        virtual ~MatrixInterface () = default;
+public:
+  virtual ~MatrixInterface() = default;
 
-        Matrix operator-() const;
-        Matrix operator+() const;
-        void operator=(const Matrix&);
+  MatrixInterface<T> operator-(const MatrixInterface<T>&) const;
+  MatrixInterface<T> operator+(const MatrixInterface<T>&) const;
+  void operator+=(const MatrixInterface<T>&);
+  void operator-=(const MatrixInterface<T>&);
+  void operator*=(const MatrixInterface<T>&);
+  void operator/=(const MatrixInterface<T>&);
 
+  bool operator<(const MatrixInterface<T>&) const;
+  bool operator>(const MatrixInterface<T>&) const;
+  bool operator>=(const MatrixInterface<T>&) const;
+  bool operator<=(const MatrixInterface<T>&) const;
+  bool operator==(const MatrixInterface<T>&) const;
 
+  MatrixInterface<T>& operator[](size_t col);
 
-      
-    private:
-      // Arithmetic
-      virtual void __add__(const Matrix&) = 0;
-      virtual void __stdMult__(const Matrix&) = 0;
+  // Type casting
+  T operator float() const;
 
-      // Data structure
-      virtual void __copy__(const Matrix&) = 0;
+  // Operations
+  MatrixInterface<T> operator!();
+  MatrixInterface<T> operator*();
+  MatrixInterface<T> operator~();
 
-      virtual void __deep_copy__(const Matrix& input) { __copy__(input); }
-      virtual void __shallow_copy__(const Matrix& input) { __copy__(input); }
+  // Equality
+  void operator=(const MatrixInterface<T>&);
 
-      // Relational 
-      virtual bool __compare_greater_than__(const Matrix&) = 0;
-      virtual bool __compare_less_than__(const Matrix&) = 0;
-      virtual bool __compare_greater_or_equal__(const Matrix&) = 0;
-      virtual bool __compare_less_or_equal__(const Matrix&) = 0;
-      virtual bool __compare_equal__(const Matrix&) = 0;
-      virtual bool __compare_weak_equal__(const Matrix&) = 0;
+  // get the dimension
+  inline dim get_dimension() { return dim; }
 
-      // Matrix operations
-      virtual T __trace__ () = 0;
+protected:
+  dim dimension;
 
-      virtual bool __to_inverse__() = 0;
-      virtual Matrix __inverse__() = 0;
+private:
+  // Arithmetic
+  virtual void __add__(const MatrixInterface<T>&) = 0;
+  virtual void __stdMult__(const MatrixInterface<T>&) = 0;
 
-      virtual bool __to_transpose__() = 0;
-      virtual Matrix __transpose__ () = 0;
+  // Data structure
+  virtual void __copy__(const MatrixInterface<T>&) = 0;
+
+  virtual void __deep_copy__(const MatrixInterface<T>& input)
+  {
+    __copy__(input);
+  }
+  virtual void __shallow_copy__(const MatrixInterface<T>& input)
+  {
+    __copy__(input);
+  }
+
+  // Relational
+  virtual bool __compare_greater_than__(const MatrixInterface<T>&) = 0;
+  virtual bool __compare_less_than__(const MatrixInterface<T>&) = 0;
+  virtual bool __compare_greater_or_equal__(const MatrixInterface<T>&) = 0;
+  virtual bool __compare_less_or_equal__(const MatrixInterface<T>&) = 0;
+  virtual bool __compare_equal__(const MatrixInterface<T>&) = 0;
+  virtual bool __compare_weak_equal__(const MatrixInterface<T>&) = 0;
+
+  // MatrixInterface operations
+  virtual T __trace__() = 0;
+
+  virtual bool __to_inverse__() = 0;
+  virtual MatrixInterface<T> __inverse__() = 0;
+
+  virtual bool __to_transpose__() = 0;
+  virtual MatrixInterface<T> __transpose__() = 0;
+
+  // Accessors
+  virtual const MatrixInterface<T>& get_row(size_t index) const = 0;
+  virtual const MatrixInterface<T>& get_col(size_t index) const = 0;
+  virtual const T& get(size_t row, size_t col) const = 0;
 };
 
 #endif /* end of include guard MATRIXINTERFACE_HPP */
-
--- include/linearalg/matLib.hpp
+++ include/linearalg/matLib.hpp
@@ -3,96 +3,10 @@
 #include <iostream>
 #include <vector>
 
-// Matlib used for matrix calculations and operations
 namespace matlib {
-// A matrix is a vector of floats
+/*
+ * Matrix algorithms
+ */
 
-template <typename T>
-class Matrix
-{
-public:
-
-  Matrix();                  
-
-  Matrix(const std::string&); 
-
-  Matrix(const size_t, const size_t);           
-
-  Matrix(const size_t,
-         const size_t,
-         const T); 
-
-  virtual ~Matrix() = default;
-
-  int dim1; // rows
-  int dim2; // cols
-
-
-
-private:
-  void print(); // handy print function
-
-  void insert(int, int, const T&); 
-  float get(const size_t, const size_t);
-
-  // common matrix functions
-  T normEuclid(); // euclidian / frobenius norm
-  T getMax();     // maximum value (not index)
-  int getMaxIndex();  // maximum index
-
-public:
-  //================================Operators==================================
-  // unary opperators
-  Matrix operator-();
-  Matrix operator=(const float*);
-
-  // binary operrators
-  Matrix operator+(Matrix const&);
-  Matrix operator-(Matrix const&);
-  Matrix operator*(Matrix const&);
-  Matrix operator*(float const&);
-  Matrix operator+=(Matrix const&);
-  Matrix operator-=(Matrix const&);
-  Matrix operator*=(Matrix const&);
-
-  // Relational operators
-  bool operator<(Matrix const&);
-  bool operator>(Matrix const&);
-  bool operator==(Matrix const&);
-  bool operator<=(Matrix const&);
-  bool operator>=(Matrix const&);
-
-  // type casting
-  operator float();
-
-  Matrix operator!(); // inverse (!tensor)
-  Matrix operator*(); // transpose (*tensor)
-  float operator~();  // trace (~tensor)
-
-private:
-  std::vector<std::vector<T>> arr; // A matrix
-};
-
-
-namespace linfunc {
-void parAdd(const Matrix&, Matrix&); // Assembly like , add const to mutable
-T innerProd(const Matrix&, const Matrix&);
-Matrix augment(const Matrix&, const Matrix&);
-Matrix getRow(const Matrix&, size_t);
-Matrix getCol(const Matrix&, size_t);
-bool swapRow(Matrix&, size_t, size_t);
-bool swapCol(Matrix&, size_t, size_t);
-bool replaceRow(Matrix&, const Matrix&, size_t);
-bool toGaussJordan(Matrix&);
-bool toGaussJordanRREF(Matrix&);
-Matrix GaussJordan(Matrix&);
-Matrix GaussJordanRREF(Matrix&);
-Matrix copy_std_mult(const Matrix&, const Matrix&);
-bool scalar_mult(const Matrix&, Matrix&);
-bool is_singular(const Matrix&);
-Matrix transpose(const Matrix&);
-Matrix inverse(const Matrix&);
-T trace(const Matrix&);
-}
 }
 #endif
